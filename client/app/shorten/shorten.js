14c9c593a8ef572694498bee4df5da35946aeafb
trinile <trinile@gmail.com>
2016-04-01 15:10:34 -0700

    Add routing for links and shorten paths

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,4 +2,10 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
+  $scope.link = {};
+
+  $scope.addLink = function(linkObj) {
+    return Links.addOne(linkObj);
+  };
+
 });
