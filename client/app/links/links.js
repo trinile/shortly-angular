59ea23385e5aedb4d6e0e0835d11a1c2ace72130
trinile <trinile@gmail.com>
2016-04-01 14:52:11 -0700

    Updata Links controller with data object and links property

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,9 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  $scope.data = {};
+
+  Links.getAll().then(function(links) {
+    $scope.data.links = links;
+  });
 });
